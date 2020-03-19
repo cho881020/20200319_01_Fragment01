992d81e5ca54fdc5111e9fe109c7fcf16c5ec73c
cho881020 <kj_cho@nepp.kr>
2020-03-19 12:00:42 +0900

    두번째 프래그먼트 데이터바인딩 세팅

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/SecondFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/SecondFragment.java
@@ -7,21 +7,27 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import kr.co.tjoeun.a20200319_01_fragment01.R;
+import kr.co.tjoeun.a20200319_01_fragment01.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
+
+    FragmentSecondBinding binding = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
 
 
     }
