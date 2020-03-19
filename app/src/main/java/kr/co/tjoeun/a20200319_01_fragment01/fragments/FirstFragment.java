874f78328fbf38541be40b42167da4b77c43edd2
cho881020 <kj_cho@nepp.kr>
2020-03-19 11:06:58 +0900

    프래그먼트 동작 코드 작성  - getActivity() 활용!

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/FirstFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/FirstFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import kr.co.tjoeun.a20200319_01_fragment01.MainActivity;
 import kr.co.tjoeun.a20200319_01_fragment01.R;
 import kr.co.tjoeun.a20200319_01_fragment01.databinding.FragmentFirstBinding;
 
@@ -20,13 +22,24 @@ public class FirstFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
-         return binding.getRoot();
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        binding.infoToastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.nameEdt.getText().toString();
+                String phone = binding.phoneEdt.getText().toString();
+
+                Toast.makeText(getActivity(), String.format("%s : %s", name, phone), Toast.LENGTH_SHORT).show();
+                
+            }
+        });
 
     }
 }
